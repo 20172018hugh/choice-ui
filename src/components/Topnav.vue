c9a416897a0696fb0cfdf9290704d561239e2e4e
20172018hugh <1772796367@qq.com>
2022-10-16 20:32:25 +0800

    feat:实现aside切换

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,30 +1,50 @@
 <template>
     <div class="topnav">
-<div class="logo">LOGO</div>
-<ul class="menu">
-<li>菜单1</li>
-<li>菜单2</li>
-</ul>
-</div>
+        <div class="logo" @click="toggleMenu">LOGO</div>
+        <ul class="menu">
+            <li>菜单1</li>
+            <li>菜单2</li>
+        </ul>
+    </div>
 </template>
-
+<script lang="ts">
+import { inject, Ref } from 'vue'
+export default {
+    name: 'Topnav',
+    setup() {
+        const menuVisiable = inject<Ref<boolean>>('menuVisiable');
+        console.log('topnav的inject值:', menuVisiable.value);
+        const toggleMenu = () => {
+            menuVisiable.value = !menuVisiable.value
+        }
+        return {
+            toggleMenu,
+            menuVisiable,
+        }
+    }
+}
+</script>
 <style lang="scss" scoped>
 .topnav {
-background: pink;
-display: flex;
-padding: 16px;
-> .logo {
-max-width: 6em;
-margin-right: auto;
-}
-> .menu {
-display: flex;
-white-space: nowrap;
-flex-wrap: nowrap;
-> li {
-margin: 0 1em;
-}
-}
-}
+    background: pink;
+    display: flex;
+    padding: 16px;
+    position: relative;
+    z-index: 10;
 
+    >.logo {
+        max-width: 6em;
+        margin-right: auto;
+    }
+
+    >.menu {
+        display: flex;
+        white-space: nowrap;
+        flex-wrap: nowrap;
+
+        >li {
+            margin: 0 1em;
+        }
+    }
+}
 </style>
