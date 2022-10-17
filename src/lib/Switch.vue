4a3739784342eab3b0d0e9f97a30956c5784b50a
20172018hugh <1772796367@qq.com>
2022-10-17 15:41:51 +0800

    feat:switch组件初始化和基本样式调整

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>
-        switch组件
-    </div>
+    <button><span></span></button>
 </template>
 <script lang="ts">
 export default {
     name: 'Switch'
 }
 </script>
-<style lang="">
-    
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+
+button {
+    height: $h;
+    width: $h*2;
+    border: none;
+    border-radius: $h/2;
+    background: blue;
+    position: relative;
+}
+
+span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    border-radius: $h2/2;
+    background: white;
+}
 </style>
