04326d856c0c7ff027faa4d5b233caaff9204dad
20172018hugh <1772796367@qq.com>
2022-10-18 17:46:03 +0800

    feat:初始化button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-        button组件
-    </div>
+    <div>Button示例</div>
+    <h1>示例1</h1>
+    <Button @click="onClick">你好</Button>
 </template>
 <script>
+import Button from '../lib/Button.vue'
 export default {
-    name: 'ButtonDemo',
+    name: "ButtonDemo",
+    components: { Button },
+    setup() {
+        const onClick = () => {
+            console.log('点击了button')
+        }
+        return {
+            onClick,
+        }
+    }
 }
 </script>
 <style lang="">
