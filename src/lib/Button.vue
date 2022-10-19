d0e4024852488967182f204ee8c0a7994ffd2e62
20172018hugh <1772796367@qq.com>
2022-10-19 18:07:39 +0800

    支持level属性及基本样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -10,7 +10,8 @@ export default {
     // 实现属性分别绑定，按需绑定
     props: {
         theme: { type: String, default: 'button' },
-        size: { type: String, default: 'normal' }
+        size: { type: String, default: 'normal' },
+        level: { type: String, defatul: 'normal' },
     },
     setup(props) {
         // const { size, ...rest } = context.attrs;
@@ -21,6 +22,7 @@ export default {
         const classes = computed(() => ({
             [`choice-theme-${props.theme}`]: props.theme,
             [`choice-size-${props.size}`]: props.size,
+            [`choice-level-${props.level}`]: props.level,
         }))
         return {
             classes,
@@ -34,6 +36,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: #f56c6c;
 
 .choice-button {
     box-sizing: border-box;
@@ -49,6 +52,7 @@ $radius: 4px;
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
 
     &+& {
         margin-left: 8px;
@@ -101,6 +105,63 @@ $radius: 4px;
         font-size: 12px;
         height: 20px;
         padding: 0 4px;
+    }
+
+    &.choice-theme-button {
+        &.choice-level-main {
+            background: $blue;
+            color: white;
+            border-color: $blue;
+
+            &:hover,
+            &:focus {
+                background: darken($blue, 10%);
+                border-color: darken($blue, 10%);
+            }
+        }
+
+        &.choice-level-danger {
+            background: $red;
+            border-color: $red;
+            color: white;
+
+            &:hover,
+            &:focus {
+                background: darken($red, 10%);
+                border-color: darken($red, 10%);
+            }
+        }
+    }
+
+    &.choice-theme-link {
+        &.choice-level-danger {
+            color: $red;
+
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
+    }
+
+    &.choice-theme-text {
+        &.choice-level-main {
+            color: $blue;
+
+            &:hover,
+            &:focus {
+                color: darken($blue, 10%);
+            }
+        }
+
+        &.choice-level-danger {
+            color: $red;
+
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
     }
 }
 </style>
