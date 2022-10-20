196080d4572bae10c2dcaacf24f315150e235703
20172018hugh <1772796367@qq.com>
2022-10-20 16:10:16 +0800

    初始化dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
-        dialog组件
-    </div>
+    <div>Dialog 示例</div>
+    <h1>示例1</h1>
+    <Dialog></Dialog>
 </template>
-<script>
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
 export default {
     name: 'DialogDemo',
+    components: { Dialog }
 }
 </script>
 <style lang="">
